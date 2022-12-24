04eab28f0c1dd8e0a6b721ada5d02e2902cc2751
Mahmoud ElGharably <elgharably@gmail.com>
2022-12-24 20:22:57 +0200

    First-Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BooksModule } from './books/books.module';
+import { PhotosComponent } from './components/photos/photos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BooksModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
